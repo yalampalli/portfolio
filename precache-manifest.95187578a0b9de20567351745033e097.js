3177b9a5ba2c0c2a13c282020cd26090a9faa458
yalampalli <yeshwanth.alampalli@gmail.com>
2019-08-04 15:01:32 +0530

    Updates

---
+++ precache-manifest.95187578a0b9de20567351745033e097.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9fb8c50d1db7b5337461f0ee2d9cc7f0",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "b4a0ab59b94125b1c1f8",
+    "url": "/portfolio/static/css/main.4ecc39f4.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/portfolio/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "b4a0ab59b94125b1c1f8",
+    "url": "/portfolio/static/js/main.03e6e26d.chunk.js"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  },
+  {
+    "revision": "ff79db2c315b6a7c57b68ab275f2d8dd",
+    "url": "/portfolio/static/media/me.ff79db2c.jpg"
+  }
+]);
